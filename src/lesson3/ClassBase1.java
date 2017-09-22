f2dc05976a92bde88e784c0cdaf3419109a6a870
skpro <skprojectkang@gmail.com>
2017-09-22 13:51:05 +0900

    Hello world create

--- src/lesson3/ClassBase1.java
+++ src/lesson3/ClassBase1.java
@@ -1,18 +1,15 @@
 package lesson3;
 
-
 public class ClassBase1 {
     static public void main(String[] arg){
         Counstructor c = new Counstructor();
         System.out.println(c.getName());
-
+        System.out.println(c.getFriend(0));
         Counstructor c1 = new Counstructor("shinkang");
         System.out.println(c1.getName());
-
-        Counstructor c2 = new Counstructor("shin","kang");
+        System.out.println(c1.getFriend(0));
+        Counstructor c2 = new Counstructor("shinkang", "kang");
         System.out.println(c2.getName());
+        System.out.println(c2.getFriend(0));
     }
-
-
-
 }
--- src/lesson3/Counstructor.java
+++ src/lesson3/Counstructor.java
@@ -1,24 +1,36 @@
 package lesson3;
 
+import java.util.ArrayList;
+
 public class Counstructor {
 
     private String firstName;
     private String lastName;
+    private String seperator;
+    private ArrayList<String> friends;
+    {
+        seperator = " ";
+    }
+    {
+        friends = new ArrayList<>();
+        friends.add("mike");
+        friends.add("jane");
+    }
 
     public Counstructor(){
-        this("john","doe");
+        this("john", "doe");
     }
-
     public Counstructor(String fname){
-        this(fname,"doe");
+        this(fname, "doe");
     }
-
     public Counstructor(String fname, String lname){
         firstName = fname;
         lastName = lname;
     }
-
-    public String getName() {
-        return firstName + " " + lastName;
+    public String getName(){
+        return firstName + seperator + lastName;
+    }
+    public String getFriend(int index){
+        return friends.get(index);
     }
 }
