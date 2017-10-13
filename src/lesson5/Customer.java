5d7848806a3c5c320cae07932a6b9a86abb6bef7
philoskophy <skprojectkang@gmail.com>
2017-10-13 13:24:20 +0900

    5강 은행놀이 강타입의 자바

--- src/lesson5/Customer.java
+++ src/lesson5/Customer.java
@@ -3,33 +3,31 @@ package lesson5;
 public class Customer {
     static class Deposit implements DepositRequest{
 
-        private final Customer = c;
-        private final Money = g;
-        private final Account = a;
+        private final Customer customer;
+        private final Money money;
+        private final Account account;
 
-        Deposit(Customer c, Money g, Account a){
-            this.Customer = c;
-            this.Money = g;
-            this.Account = a;
+        Deposit(Customer c, Money m, Account a){
+            this.customer = c;
+            this.money = m;
+            this.account = a;
         }
         @Override
         public Account getDepositAccount() {
-            return this.Customer;
+            return this.account;
         }
 
         @Override
         public Customer getCustomer() {
-            return this.Money;
+            return this.customer;
         }
 
         @Override
         public Money getMoney() {
-            return this.Account;
+            return this.money;
         }
     }
-    public boolean deposit(Banker b, Currency c, int g){
-        return b.requestDeposit(new Money(c, g));
-
-
+    public boolean deposit(Banker b, Currency c, int q){
+        return b.requestDeposit(new Deposit(this, new Money(c, q), new Account()));
     }
 }
