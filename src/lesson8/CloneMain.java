88bfdb1b3b83c52f02aed63dd64523020062433d
philoskophy <skprojectkang@gmail.com>
2017-11-10 14:08:02 +0900

    8강
    이론수업 종료/ 결과값이 선생님과 다르게 나옴 확인해보자.

--- src/lesson8/CloneMain.java
+++ src/lesson8/CloneMain.java
@@ -1,7 +1,21 @@
 package lesson8;
 
 public class CloneMain {
+//결과값으로 0이 출력됨 / 20이 출력이 되어야 성공한 것 . 왜 0 이 뜨는지 모르겠음.
+    static public void main(String[] arg){
+        Base b1 = new Base(10);
+        Owner o1 = new Owner(10,b1);
 
-    static public void main(String[] arg){}
+        //Owner o2 = o1.clone();   //유일자원을 물고 있으면 클론을 하면 안된다.
+        Owner o2 = null;
+        try {
+            o2 = o1.clone();
+        } catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+        Base b2 = o1.getBase();
+        b2.baseValue = 20;
+        System.out.println(o2.getBase().baseValue);
+    }
 
 }
--- src/lesson8/Owner.java
+++ src/lesson8/Owner.java
@@ -1,4 +1,26 @@
 package lesson8;
 //가시성을 낮춰 - 간섭하는 애들이 줄어든다.
-public class Owner {
+
+class Owner {
+    private final int ownerValue;
+    private final Base ownerBase;
+
+     Owner(int ownerValue, Base ownerBase) {
+       this.ownerValue = ownerValue;
+        this.ownerBase = ownerBase;
+    }
+
+
+    @Override
+    protected Owner clone() throws CloneNotSupportedException {
+         //얕은복사
+        Owner result = new Owner(this.ownerValue,this.ownerBase);
+        //깊은복사
+        result = new Owner(this.ownerValue,this.ownerBase.clone());
+        return result;
+    }
+
+    public Base getBase() {
+         return ownerBase;
+    }
 }
