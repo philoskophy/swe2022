ae724cce7b215c1e603d1cd9c4c80066a2646107
philoskophy <skprojectkang@gmail.com>
2017-11-12 19:10:08 +0900

    과제 _
    TodoList 만들기
    
    List 와 Task 사이를 어떻게 구분해야하나.
    ArrayList를 만드는게 맞는가?
    
    ----추가
    
    Task 관련

--- src/com/philoskophy/todo/TodoTask.java
+++ src/com/philoskophy/todo/TodoTask.java
@@ -2,8 +2,12 @@ package com.philoskophy.todo;
 
 public class TodoTask {
 
+    int priority ;
+    boolean done ;
+// 배열에 어떻게 이것을 할당하는가?
+    //애초에 배열로 하는게 맞는가?
 
     public void changeName(){
-
+// 여기있는게 맞는건가 헷갈림...
     }
 }
