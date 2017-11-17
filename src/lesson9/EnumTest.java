40dc8723a2501abfe0e9f3b0378c383d26daec41
philoskophy <skprojectkang@gmail.com>
2017-11-17 14:27:53 +0900

    9강 1. ENUM 짱. 2. 일단 FINAL로 선언하고 바꿔야할 일이 있으면 그때 수정해봐라.

---
+++ src/lesson9/EnumTest.java
@@ -0,0 +1,22 @@
+package lesson9;
+
+public class EnumTest {
+    public static class Tshirt{
+        public enum Size {SMALL, MEDIUM, LARGE} // 사람마다 이상한 싸이즈를 만들어 쓰는게 아니라, 정해진 싸이즈만 사용할 수 있게 클래시가 아닌 enum으로 만든다.
+
+        private final Size size;
+
+        public Tshirt(Size $size){
+            final Size size = $size;
+            this.size = size;
+        }
+//$를 이쁘게? 하려고 붙이는건데 아직 확실하게 잘 모르겠음
+        public boolean isFit(Person target){
+            final int base = target.height/2 ;
+            switch(this.size){
+                case SMALL : return base < 50 ;
+                case MEDIUM: return base < 70 ;
+            }
+        }
+    }
+}
